5f2ad1ed4baab978a55c3c6578c81f21bb34f7e7
Arthur Nisnevich <arthur.nisnevich@gmail.com>
2011-07-07 15:17:00 -0600

    Shortened URL resolution

--- src/com/linter/LintedPage.java
+++ src/com/linter/LintedPage.java
@@ -1,12 +1,73 @@
 package com.linter;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
 public class LintedPage {
-	String _originalUrl;
+	static private Logger logger = Logger.getLogger(LintedPage.class);
 	
-	// resolved URL
-	// page title
+	String _originalUrl;
+	String[] _aliases = {};
 	
 	public LintedPage(String originalUrl) {
 		_originalUrl = originalUrl;
+	}
+	
+	/***
+	 * Process the original URL associated with this LintedPage
+	 */
+	public void process() {
+		logger.info("Processing URL: " + _originalUrl);
+		
+		logger.debug("Expanding any shortened URLs...");
+		ArrayList<String> aliases = Linter.expandShortenedUrls(_originalUrl);
+		if (aliases != null && aliases.size() > 0)
+			_aliases = aliases.toArray(new String[0]);
+	}
+	
+	/***
+	 * Returns the original URL (before any shortened URLs were expanded)
+	 * @return
+	 */
+	public String getOriginalUrl() {
+		return _originalUrl;
+	}
+	
+	/***
+	 * Gets any aliases for this URL -- e.g. any redirects between the original URL and its actual destination
+	 * @return
+	 */
+	public String[] getAliases() {
+		return _aliases;
+	}
+	
+	/***
+	 * Gets the final destination, after expanding any shortened URLs starting from the original URL
+	 * @return
+	 */
+	public String getDestinationUrl() {
+		if (_aliases.length == 0)
+			return _originalUrl;
+		else
+			// The last alias should be the final URL
+			return _aliases[_aliases.length - 1];
+	}
+	
+	public String toDebugString() {
+		StringBuilder sb = new StringBuilder(_originalUrl);
+		sb.append(" {\n");
+		sb.append("\tALIASES:");
+			if (_aliases.length == 0)
+				sb.append("\tNONE\n");
+			else {
+				sb.append('\n');
+				for (String alias : _aliases) {
+					sb.append("\t\t"); sb.append(alias); sb.append('\n');
+				}
+			}
+		sb.append("\tDESTINATION URL:\t\t"); sb.append(this.getDestinationUrl()); sb.append('\n');
+		sb.append("}\n");
+		return sb.toString();
 	}
 }
--- src/com/linter/Linter.java
+++ src/com/linter/Linter.java
@@ -1,9 +1,87 @@
 package com.linter;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
 public class Linter {
+	static private Logger logger = Logger.getLogger(Linter.class);
+	
+	public static final int HTTP_CONNECT_TIMEOUT = 5000;	// 5 sec
+	
+	
+	/***
+	 * Run Linter as a command-line to optionally test URLs via CLI
+	 * @param args The URLs to resolve
+	 */
+	public static void main(String[] args) {		
+		if (args.length == 0) {
+			System.out.println("Invalid number of arguments. You must include at least one URL to process.");
+			System.exit(1);
+		}
+		
+		logger.info("Running Linter");
+		
+		LintedPage lp;
+		for (int i = 0; i < args.length; i++)
+		{
+			lp = Linter.processUrl(args[i]);
+			System.out.println(lp.toDebugString());
+		}
+	}
+	
 	public static final LintedPage processUrl(String url) {
 		LintedPage lp = new LintedPage(url);
-
+		lp.process();
 		return lp;
+	}
+	
+	/***
+	 * Follows URLs starting from address and returns a list of aliases (hops), with the last one being the destination. If the returned
+	 * ArrayList of aliases has no entries, then the source address is the destination address. This method will return null
+	 * on an error.
+	 * @param address - The source URL 
+	 * @return an ArrayList of aliases from the source URL to the destination (including the destination)
+	 */
+	public static ArrayList<String> expandShortenedUrls(String address) {
+		ArrayList<String> aliases = new ArrayList<String>();
+		
+		String locationRedirect = address;
+		
+		while (locationRedirect != null) {
+			try {
+				URL url = new URL(locationRedirect);
+				
+				logger.trace("Following " + locationRedirect + "...");
+				
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
+				connection.setInstanceFollowRedirects(false);
+				connection.setRequestMethod("HEAD");
+				connection.setConnectTimeout(Linter.HTTP_CONNECT_TIMEOUT);
+				connection.connect();
+				
+				locationRedirect = connection.getHeaderField("Location");
+				if (locationRedirect != null) {
+					logger.debug("Discovered redirect to " + locationRedirect);
+					aliases.add(locationRedirect);
+				} else {
+					logger.debug("URL resolved to its destination");
+				}
+				connection.disconnect();
+			} catch (MalformedURLException ex) {
+				logger.error("Invalid URL [" + locationRedirect + "]: " + ex.getMessage());
+				return null;
+			} catch (IOException ioe) {
+				logger.error("IO Exception [" + locationRedirect + "]: " + ioe.getMessage());
+				return null;
+			}
+		}
+		
+		return aliases;
 	}
 }
