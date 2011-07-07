1c7361eb2e16ef41ef488833f97e78dc46f89bce
Arthur Nisnevich <arthur.nisnevich@gmail.com>
2011-07-07 13:50:13 -0600

    Initial commit

---
+++ src/com/linter/LintedPage.java
@@ -0,0 +1,12 @@
+package com.linter;
+
+public class LintedPage {
+	String _originalUrl;
+	
+	// resolved URL
+	// page title
+	
+	public LintedPage(String originalUrl) {
+		_originalUrl = originalUrl;
+	}
+}
---
+++ src/com/linter/Linter.java
@@ -0,0 +1,9 @@
+package com.linter;
+
+public class Linter {
+	public static final LintedPage processUrl(String url) {
+		LintedPage lp = new LintedPage(url);
+
+		return lp;
+	}
+}
