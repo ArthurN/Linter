e42141116dec1fe923a39a17881b38e4b6e6598b
Arthur Nisnevich <arthur.nisnevich@gmail.com>
2011-07-07 19:51:20 -0600

    Cleanup, add favicon support, and parse OK status

--- src/com/linter/LintedPage.java
+++ src/com/linter/LintedPage.java
@@ -3,6 +3,7 @@ package com.linter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.htmlcleaner.HtmlCleaner;
@@ -11,20 +12,36 @@ import org.htmlcleaner.TagNode;
 public class LintedPage {
 	static private Logger logger = Logger.getLogger(LintedPage.class);
 	
-	String _originalUrl;
-	String[] _aliases = {};
-	String _title;
-	String _description;
-	TagNode _node;
+	private boolean _parseOk = false;
+	private String _originalUrl;
+	private String[] _aliases = {};
+	private String _title;
+	private String _description;
+	private String _favIconUrl;
+	
+	private long _processingTime;
+	
+	private TagNode _node;
 	
 	public LintedPage(String originalUrl) {
 		_originalUrl = originalUrl;
 	}
 	
+	void process() {
+		final long startTime = System.nanoTime();
+		final long endTime;
+		try {
+			processRunner();
+		} finally {
+		  endTime = System.nanoTime();
+		}
+		_processingTime = endTime - startTime;
+	}
+	
 	/***
 	 * Process the original URL associated with this LintedPage
 	 */
-	public void process() {
+	protected void processRunner() {
 		logger.info("Processing URL: " + _originalUrl);
 		
 		logger.debug("Expanding any shortened URLs...");
@@ -51,7 +68,7 @@ public class LintedPage {
 				_title = ((TagNode)titleNodes[0]).getText().toString().trim();
 			}
 		} catch (Exception e) {
-			logger.warn("Could not extract the page title: " + e.toString());
+			logger.warn("Error extracting the page title: " + e.toString());
 		}
 		
 		// Page description
@@ -61,8 +78,28 @@ public class LintedPage {
 				_description = ((TagNode)descNodes[0]).getAttributeByName("content").trim();
 			}
 		} catch (Exception e) {
-			logger.warn("Could not extract the page description: " + e.toString());
+			logger.warn("Error extracting the page description: " + e.toString());
 		}
+		
+		// Fav icon
+		try {
+			Object[] favIconNodes = _node.evaluateXPath("//head/link[@rel='icon']");
+			if (favIconNodes != null) {
+				_favIconUrl = ((TagNode)favIconNodes[0]).getAttributeByName("href").trim();
+			}
+		} catch (Exception e) {
+			logger.warn("Error extracting the favicon URL: " + e.toString());
+		}
+		
+		_parseOk = true;
+	}
+	
+	/***
+	 * Whether or not the parse completed successfully
+	 * @return
+	 */
+	public Boolean getParseOk() {
+		return _parseOk;
 	}
 	
 	/***
@@ -109,9 +146,41 @@ public class LintedPage {
 		return _description;
 	}
 	
+	/***
+	 * Gets the page's favicon (from <link rel='icon'>)
+	 * @return
+	 */
+	public String getFavIconUrl() {
+		return _favIconUrl;
+	}
+	
+	/***
+	 * Gets the processing time in milliseconds
+	 * @return
+	 */
+	public long getProcessingTimeMillis() {
+		return TimeUnit.NANOSECONDS.toMillis(_processingTime);
+	}
+	
+	/***
+	 * Gets the processing time in human-readable format
+	 * @return
+	 */
+	public String getProcessingTimeForHumans() {
+
+		// mm:ss is:
+		//	    TimeUnit.NANOSECONDS.toMinutes(_processingTime),
+		//	    TimeUnit.NANOSECONDS.toSeconds(_processingTime) - 
+		//	    TimeUnit.MINUTES.toSeconds(TimeUnit.NANOSECONDS.toMinutes(_processingTime))
+		
+		Float millis = new Float(getProcessingTimeMillis() / 1000);
+		return String.format("%.4f", millis);
+	}
+	
 	public String toDebugString() {
 		StringBuilder sb = new StringBuilder(_originalUrl);
 		sb.append(" {\n");
+		sb.append("\tPARSE OK:\t\t"); sb.append(this.getParseOk()); sb.append('\n');
 		sb.append("\tALIASES:");
 			if (_aliases.length == 0)
 				sb.append("\t\tNONE\n");
@@ -124,7 +193,8 @@ public class LintedPage {
 		sb.append("\tDEST URL:\t\t"); sb.append(this.getDestinationUrl()); sb.append('\n');
 		sb.append("\tPAGE TITLE:\t\t"); sb.append(this.getTitle()); sb.append('\n');
 		sb.append("\tDESCRIPTION:\t\t"); sb.append(this.getDescription()); sb.append('\n');
-		sb.append("}\n");
+		sb.append("\tFAV ICON:\t\t"); sb.append(this.getFavIconUrl()); sb.append('\n');
+		sb.append("} in "); sb.append(this.getProcessingTimeForHumans()); sb.append(" s\n");
 		return sb.toString();
 	}
 }
